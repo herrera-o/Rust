41ba8ce9fd4bbcf768dafa78cf37ad9e2e4375f4
Oscar Herrera <herrera_o@outlook.com>
2019-03-21 16:26:14 -0700

    Fixed misspellings and other errors.

--- src/datastructures/linked_list.rs
+++ src/datastructures/linked_list.rs
@@ -2,14 +2,14 @@ use std::rc::Rc;
 use std::boxed::Box;
 use std::cell::RefCell;
 
-#[derive(debug)]
+#[derive(Debug)]
 pub struct Node<T> {
     pub data: T,
-    pub next: Option<Rc<Refcell<Box<Node<T>>>>>,
+    pub next: Option<Rc<RefCell<Box<Node<T>>>>>,
 }
 
 impl<T> Node<T> {
-    fn new(input: T) -> Refcell<Box<Node<T>>> {
+    fn new(input: T) -> RefCell<Box<Node<T>>> {
         RefCell::new(Box::new(Node {
             data: input,
             next: None,
@@ -19,8 +19,8 @@ impl<T> Node<T> {
 
 #[derive(Debug)]
 pub struct LinkedList<T> {
-    head: Option<Rc<Refcell<Box<Node<T>>>>>,
-    tail: Option<Rc<Refcell<Box<Node<T>>>>>,
+    head: Option<Rc<RefCell<Box<Node<T>>>>>,
+    tail: Option<Rc<RefCell<Box<Node<T>>>>>,
     count: isize,
 }
 
@@ -29,27 +29,27 @@ impl<T> LinkedList<T> {
         LinkedList {
             head: None,
             tail: None,
-            count: 0;
+            count: 0,
         }
     }
 
     pub fn addlast(&mut self, input: T) {
         let newnode: Rc<RefCell<Box<Node<T>>>> = Rc::new(Node::new(input));
 
-        if self.size == 0 {
+        if self.count == 0 {
             self.tail = Some(Rc::clone(&newnode));
             self.head = Some(Rc::clone(&newnode));
             self.count += 1
         } else {
             match &mut self.tail {
                 Some(ref mut s) => {
-                    let mut = s;
-                    (*s).next = Some(Rc::clone(&newnode));
-                    s = Some(Rc::clone(&newnode));
-                    self.count += 1;
+                    let mut oldnode = (*s).borrow_mut();
+                    (*oldnode).next = Some(Rc::clone(&newnode));
                 },
                 None => (),
             }
+            self.tail = Some(Rc::clone(&newnode));
+            self.count += 1;
         }
     }
 
@@ -64,8 +64,27 @@ impl<T> LinkedList<T> {
                 },
                 None => return None,
             };
-            
+
             ptr
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn addlast_get_item() {
+        LinkedList list = new LinkedList();
+        list.addlast(5);
+        list.addlast(3);
+        list.addlast(9);
+        list.addlast(10);
+        list.addlast(77);
+
+        let item1 = list.get_item(1);
+        let item1 = a.unwrap();
+        let item1 = a.borrow();
+
+        assert_eq!((*item1).data, 5);
     }
 }
